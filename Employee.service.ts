b9928d10022620b96ff21401e88480a8ba715b01
Mustafa Wasif <109570226+Wasif57@users.noreply.github.com>
2023-11-02 14:53:13 +0600

    Add files via upload

---
+++ Employee.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEmployeeDto } from './dto/createEmployee.dto';
+import { Employee } from './entities/employee.model';
+import { register } from 'module';
+@Injectable()
+export class EmployeeService {
+  employees: Employee[] - [] ;
+  register(createEmployeeDto : CreateEmployeeDto){
+    return this.employeeService.register(CreateEmployeeDto);
+  }
+  
+getAll(){
+  return [...this.employees];
+
+}
+find(name1 : string){
+  return this.employees.find(obj -> object.eName === name1);
+
+}
